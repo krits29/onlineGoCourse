985e722e93602651a0421a8457e6bfe9dfd6da6c
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-24 14:52:42 -0700

    Variables

---
+++ Cards/Main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var Cards String = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Ace of Clubs"  // colon not necessary
+
+	fmt.Println(card)
+}
