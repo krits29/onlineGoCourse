466ce937be594b41a46363903d2e68c961293094
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-24 15:53:42 -0700

    Slices

--- Cards/Main.go
+++ Cards/Main.go
@@ -5,11 +5,21 @@ import "fmt"
 func main() {
 	//var Cards String = "Ace of Spades"
 	card := "Ace of Spades"
-	card = "Ace of Clubs"  // colon not necessary
+	card = "Ace of Clubs" // colon not necessary
 
 	card = newCard()
 
 	fmt.Println(card)
+
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Six of Spades") // add on by recreating the slice
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
+
 }
 
 func newCard() string {
