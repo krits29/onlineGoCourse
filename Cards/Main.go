d4df5f8fac30e6e0da52307c779e0088953234aa
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-24 15:26:59 -0700

    Functions

--- Cards/Main.go
+++ Cards/Main.go
@@ -7,5 +7,11 @@ func main() {
 	card := "Ace of Spades"
 	card = "Ace of Clubs"  // colon not necessary
 
+	card = newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
