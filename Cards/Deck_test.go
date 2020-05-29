b7915d7e670a435ee72c4d7665d98f752c4b7959
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-29 14:11:07 -0700

    Testing

--- Cards/Deck_test.go
+++ Cards/Deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) { //put Test and the name of the function we aer testing
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) { //put Test and the name of the function we aer 
 	if d[len(d) - 1] != "Six of Hearts" {
 		t.Errorf("Expected last card pf Six of Hearts, but got %v", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 24 {
+		t.Errorf("Unexpected deck length, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
