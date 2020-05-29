95f3cee8472ff318c9d1c9d77bd770c28cc47509
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-29 13:34:15 -0700

    Testing

--- Cards/Deck_test.go
+++ Cards/Deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) { //put Test and the name of the function we aer 
 	if len(d) != 24 {
 		t.Errorf("Unexpected deck length, got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "Six of Hearts" {
+		t.Errorf("Expected last card pf Six of Hearts, but got %v", d[len(d) - 1])
+	}
 }
