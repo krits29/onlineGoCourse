2769094b40f4ad8b1ebb4993cc6ecc360506716d
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-29 01:40:36 -0700

    Testing

---
+++ Cards/Deck_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) { //put Test and the name of the function we aer testing
+	d := newDeck()
+
+	if len(d) != 24 {
+		t.Errorf("Unexpected deck length")
+	}
+}
