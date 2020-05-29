87c5d64f0c1bc3d85439deb325a2ef792d12b8c1
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-29 01:45:32 -0700

    Testing

--- Cards/Deck_test.go
+++ Cards/Deck_test.go
@@ -6,6 +6,6 @@ func TestNewDeck(t *testing.T) { //put Test and the name of the function we aer 
 	d := newDeck()
 
 	if len(d) != 24 {
-		t.Errorf("Unexpected deck length")
+		t.Errorf("Unexpected deck length, got %v", len(d))
 	}
 }
