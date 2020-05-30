bbb771207a17fbe7873a3a9233d1da50aa6fe5fa
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-30 12:53:00 -0700

    Declaration of Structs and Variables

--- Structs/Main.go
+++ Structs/Main.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
-	lastName string
+	lastName  string
 }
 
 func main() {
+	alex := person{"Alex", "Anderson"} //creating a new struct
 
+	//alex := person{firstName: "Alex", lastName: "Anderson"} to specify
+
+	// var alex person
+
+	fmt.Println(alex)
+
+	alex.lastName = "A" //using dots instead of getter
+
+	fmt.Printf("%+v", alex)
 }
