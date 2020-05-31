23ef15fb0ccba5f7310a882d1a8fc1664b55a10a
krits29 <31025768+krits29@users.noreply.github.com>
2020-05-31 01:10:38 -0700

    Manipulating Maps

--- Map/Main.go
+++ Map/Main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string //can skip this
+	var colors map[string]string // one way
 
-	colors = map[string]string{
+	colors = make(map[string]string)  // another way
+
+	colors = map[string]string{  //another way
 		"red":   "#ff0000",
-		"green": "#ffggg",
+		"green": "#ffggg9",
 	}
+
+	colors["white"] = "#fff778"
 
 	fmt.Println(colors)
 }
