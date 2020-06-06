f60c272ca2c1a642aa0dd7759f56cc73be2cc853
krits29 <31025768+krits29@users.noreply.github.com>
2020-06-06 11:16:47 -0700

    REST model and routes

--- Food/Main.go
+++ Food/Main.go
@@ -2,30 +2,64 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"strconv"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Food struct {
-	ID int 'json:id'
-	Title string 'json:title' 
-	URL string 'json:url' 
-	Likes int 'json:likes'
-	UserID int 'json:userid' 
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	URL    string `json:"url"`
+	Likes  int    `json:"likes"`
+	UserID int    `json:"userid"`
 }
 
 var foods []Food
 
 func main() {
 	router := mux.NewRouter()
+	router.Headers("Content-Type", "application/json",
+          "X-Requested-With", "XMLHttpRequest")
+
+	foods = append(foods, Food{1, "Pasta", "https://pasta.com", 0, 0}, Food{2, "Soup", "sdfs", 0, 0}, Food{3, "Pizza", "https://pizza.com", 0, 0})
 
 	router.HandleFunc("/foods", getFoods).Methods("GET")
-	router.HandleFunc("/foods/{id}", getFood).Methods("GET")
+	router.HandleFunc("/food/{id}", getFood).Methods("GET")
 	router.HandleFunc("/foods", addFood).Methods("POST")
 	router.HandleFunc("/foods", updateFood).Methods("PUT")
-	router.HandleFunc("/foods/{id}", removeFood).Methods("DELETE")
+	router.HandleFunc("/food/{id}", removeFood).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getFoods(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(foods)
+}
+
+func getFood(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	i,_ := strconv.Atoi(params["id"])
+
+	for _, food := range foods {
+		if food.ID == i {
+			json.NewEncoder(w).Encode(food)
+		}
+	}
+
+}
+
+func addFood(w http.ResponseWriter, r *http.Request) {
+	log.Println("Add food")
+}
+
+func updateFood(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update food")
+}
+
+func removeFood(w http.ResponseWriter, r *http.Request) {
+	log.Println("Remove book")
 }
