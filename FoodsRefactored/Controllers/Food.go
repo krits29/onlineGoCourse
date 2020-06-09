7fa83ffa534f349d3a7cca445cb18a8faedcf766
krits29 <31025768+krits29@users.noreply.github.com>
2020-06-09 00:45:17 -0700

    Refactoring into Handlers and Driver

--- FoodsRefactored/Controllers/Food.go
+++ FoodsRefactored/Controllers/Food.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
-	models "FoodsRefactored/Models"
+	"FoodsRefactored/models"
 	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //Controller is
@@ -37,6 +39,88 @@ func (c Controller) GetFoods(database *sql.DB) http.HandlerFunc {
 //what just happened here?
 //remove getFoods name to get an anonymous funciton
 //then add it to the above method to return??
+func (c Controller) GetFood(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//var food Food
+
+		food := models.Food{}
+		params := mux.Vars(r)
+
+		rows := database.QueryRow("select * from items where id=?;", params["id"])
+
+		err := rows.Scan(&food.ID, &food.Title, &food.URL, &food.Likes, &food.UserID)
+		checkErr(err)
+		// handle if no entry is found
+		json.NewEncoder(w).Encode(food)
+	}
+}
+
+func (c Controller) AddFood(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//create variable type food
+		var food models.Food
+
+		//fetching the parameter r
+		//decoding the food
+		json.NewDecoder(r.Body).Decode(&food)
+
+		//makig variable for querying into the database sqlite
+		query := "insert into items(title, url, likes, userid) values (?,?,?,?)"
+
+		//check for the error(database, statement, etc, whatever it is), and must close it afterwards
+		statement, err := database.Prepare(query)
+		checkErr(err)
+		defer statement.Close()
+
+		result, err := statement.Exec(&food.Title, &food.URL, &food.Likes, &food.UserID)
+
+		id, err := result.LastInsertId()
+		checkErr(err)
+
+		json.NewEncoder(w).Encode(id)
+	}
+}
+
+func (c Controller) UpdateFood(database *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var food models.Food
+		json.NewDecoder(r.Body).Decode(&food)
+
+		query := "update items set title = ?, url = ?, likes = ?, userid = ? where id = ?"
+
+		statement, err := database.Prepare(query)
+		checkErr(err)
+		defer statement.Close()
+
+		result, err := statement.Exec(&food.Title, &food.URL, &food.Likes, &food.UserID, &food.ID)
+
+		rowsUpdated, err := result.RowsAffected()
+
+		//id, err := result.LastInsertId()
+		checkErr(err)
+
+		json.NewEncoder(w).Encode(rowsUpdated)
+	}
+}
+
+func (c Controller) RemoveFood(database *sql.DB) http.HandlerFunc { //food := Food{}
+	return func(w http.ResponseWriter, r *http.Request) {
+		//invoing vars on mux to request the object which will return a map with key value pairs
+		params := mux.Vars(r)
+
+		statement, err := database.Prepare("delete from items where id=?;")
+		checkErr(err)
+		defer statement.Close()
+
+		result, err := statement.Exec(params["id"])
+		checkErr(err)
+
+		rowsDeleted, err := result.RowsAffected()
+		checkErr(err)
+
+		json.NewEncoder(w).Encode(rowsDeleted)
+	}
+}
 
 func checkErr(err error) {
 	if err != nil {
--- FoodsRefactored/Driver/Driver.go
+++ FoodsRefactored/Driver/Driver.go
@@ -1,13 +1,23 @@
 package driver
 
 import (
+	"FoodsRefactored/utils"
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var database *sql.DB
-
 func ConnectDB() *sql.DB {
+
+	database, err := sql.Open("sqlite3", "/Users/kritisharma/projects/Databases/foods.db")
+	utils.CheckErr(err)
+
+
+	statement, err := database.Prepare("create table if not exists items (id integer primary key, title text, url text, likes integer, userid integer)")
+	utils.CheckErr(err)
+	defer statement.Close()
+
+	statement.Exec()
+
 	return database
 }
