311afba92bc93c9a601ab0f33ec9f0fa3e84e5f9
krits29 <31025768+krits29@users.noreply.github.com>
2020-06-12 15:58:14 -0700

    Hashing Password and using tokens

--- FoodsRefactored/Utils/Utils.go
+++ FoodsRefactored/Utils/Utils.go
@@ -5,20 +5,51 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func sendError(w http.ResponseWriter, status int, err models.Error) {
+func SendError(w http.ResponseWriter, status int, err models.Error) {
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(err)
 }
 
-func sendSuccess(w http.ResponseWriter, data interface{}) {
+func SendSuccess(w http.ResponseWriter, data interface{}) {
 	json.NewEncoder(w).Encode(data)
 }
 
 func CheckErr(err error) {
 	if err != nil {
-		log.Println("Yup it returned an error", err)
+		log.Println("Yup it returned an error", errors.Wrap(err, "[1] failed with error:"))
+
 		//log.Fatal(err)
 	}
+}
+
+// HashPassword hashes password from user input
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14) // 14 is the cost for hashing the password.
+	return string(bytes), err
+}
+
+// CheckPasswordHash checks password hash and password from user input if they match
+func CheckPasswordHash(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+// EncodeAuthToken signs authentication token
+//changed uid and uint to plain
+//is that fine
+//explain?
+func EncodeAuthToken(id int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["userID"] = id
+	claims["IssuedAt"] = time.Now().Unix()
+	claims["ExpiresAt"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), claims)
+	return token.SignedString([]byte(os.Getenv("SECRET")))
 }
